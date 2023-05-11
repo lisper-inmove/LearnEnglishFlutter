74aa3b72d6ad6ecc10c54798b0caac0412279288
inmove <lisper.inmove@gmail.com>
2023-05-11 15:00:34 +0800

    Home Page With LeftTop Avatar

--- lib/home.dart
+++ lib/home.dart
@@ -16,10 +16,82 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("home-page.jpeg"),
-          fit: BoxFit.fill,
+        color: Colors.black,
+      ),
+      child: _createBody(context),
+    );
+  }
+
+  Widget _createBody(BuildContext context) {
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Expanded(
+          flex: 100,
+          child: _createHeader(context)
         ),
+        Expanded(
+          flex: 1,
+          child: _createSplit()
+        ),
+        Expanded(
+          flex: 200,
+          child: _createMain(context),
+        ),
+      ],
+    );
+  }
+
+  Widget _createSplit() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget _createHeader(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+      ),
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          Expanded(
+            flex: 20,
+            child: _createAvatar(context),
+          ),
+          Expanded(
+            flex: 80,
+            child: _createCalendar(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createAvatar(BuildContext context) {
+    return Container(
+      child: Container(
+        margin: EdgeInsets.only(left: 20, bottom: 150),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('einstein.png'),
+            fit: BoxFit.contain,
+          ),
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+
+  Widget _createCalendar(BuildContext context) {
+    return Container();
+  }
+
+  Widget _createMain(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
       ),
     );
   }
